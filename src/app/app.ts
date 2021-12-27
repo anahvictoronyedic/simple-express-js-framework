cd633ee8732b53f0e8680bc7016bf79b863238cb
Anah Victor <anahvictoronyedic@gmail.com>
2021-12-27 08:46:55 +0000

    created the bootstrap file and linked it to app.ts, created the database connection file( mysql-db.ts )

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,9 @@
 import express from "express";
+import bootstrap from "./bootstrap";
 
 const app = express();
+
+// load the bootstrap program
+bootstrap().then();
 
 export default app;
---
+++ src/app/bootstrap.ts
@@ -0,0 +1,17 @@
+
+import mySqlDb, { MySQLDB_CONFIG } from "./datastore/mysql-db";
+
+import { merge } from "lodash";
+
+export default async function(options?:{
+    mysql:MySQLDB_CONFIG,
+}){
+
+    options = merge({
+        mysql:{
+
+        } as MySQLDB_CONFIG,
+    } , options);
+    
+    await mySqlDb.load(options.mysql);
+};
