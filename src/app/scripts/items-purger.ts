e5b753ea84dc5a0c7f05701917b966f89ffa189f
Anah Victor <anahvictoronyedic@gmail.com>
2021-12-28 19:59:58 +0000

    created the items-purger script that should be run periodically

---
+++ src/app/scripts/items-purger.ts
@@ -0,0 +1,24 @@
+
+import { Script } from "../abstracts/types";
+import bootstrap from "../bootstrap";
+
+import itemsModel from "../models/items/items";
+
+/*
+Load the bootstrap program. Use mini option to indicate the application only need a little amount of resources, an economical choice
+that could save substancial money as the server scales.
+*/
+bootstrap('mini').then();
+
+export class ItemsPurger implements Script{
+
+    async run(args?: { [k: string]: any; }){
+        // clear the items that needs purging 
+        itemsModel.purgeItems();
+    }
+}
+
+const itemsPurger = new ItemsPurger;
+
+// run the script
+itemsPurger.run().then();
